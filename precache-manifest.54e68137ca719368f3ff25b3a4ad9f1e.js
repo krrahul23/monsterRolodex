790625e11bf8c9a92efe42328f0c5c2477a0d733
krrahul23 <raulk023@gmail.com>
2020-08-18 00:23:54 +0530

    Updates

---
+++ precache-manifest.54e68137ca719368f3ff25b3a4ad9f1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8fdb9036423d6e027c39586f34e5eaa",
+    "url": "/monsterRolodex/index.html"
+  },
+  {
+    "revision": "04d33dbb208375eb93d3",
+    "url": "/monsterRolodex/static/css/main.99738105.chunk.css"
+  },
+  {
+    "revision": "e26f3b49cda25c8c1e51",
+    "url": "/monsterRolodex/static/js/2.36960f10.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsterRolodex/static/js/2.36960f10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04d33dbb208375eb93d3",
+    "url": "/monsterRolodex/static/js/main.ee44cad6.chunk.js"
+  },
+  {
+    "revision": "af7cacbed46164274c44",
+    "url": "/monsterRolodex/static/js/runtime-main.3fe151d8.js"
+  }
+]);
